7e912cb3502b1378b3686686639f92b05c9b723f
= <=>
2021-06-09 16:56:13 -0500

    Manejo de estados

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+
+  state = {
+    jobTitle: 'Developer'
+  };
   handleChange = e => {
-    console.log({
-      name: e.target.name,
-      value: e.target.value,
+    
+    this.setState({
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -15,6 +19,7 @@ class BadgeForm extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
     console.log('Form was submitted');
+    console.log(this.state);
   };
 
   render() {
@@ -30,6 +35,49 @@ class BadgeForm extends React.Component {
               className="form-control"
               type="text"
               name="firstName"
+              value={this.setState.firstName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.setState.lastName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.setState.email}
+            />
+          </div>
+          <div className="form-group">
+            <label>Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.setState.jobTitle}
+
+            />
+          </div>
+          <div className="form-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.setState.twitter}
+
             />
           </div>
 
