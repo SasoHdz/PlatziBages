29f4e766df9368825263e6828d89de4263f42dc2
= <=>
2021-06-09 23:17:09 -0500

    Levantando eventos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,26 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.PureComponent{
+
+    state = { form: {
+      firstName: '',
+      lastName: '',
+      jobTitle: '',
+      email: '',
+      twitter: '',
+    }};
+
+    handleChange = e => {
+      //const nextForm = this.state.form;
+      //nextForm[e.target.name] = e.target.value;
+    
+      this.setState({
+        form:{
+          ...this.state.form,
+          [e.target.name]: e.target.value,
+        },
+      })
+    }
     render (){
         return (
             <div>
@@ -19,16 +39,19 @@ class BadgeNew extends React.PureComponent{
                 <div className="row">
                   <div className="col-6">
                     <Badge
-                      firstName="Isaac"
-                      lastName="Hernández"
-                      twitter="SasoHdz"
-                      jobTitle="Frontend Engineer"
+                      firstName={this.state.form.firstName}
+                      lastName={this.state.form.lastName}
+                      twitter={this.state.form.twitter}
+                      jobTitle={this.state.form.jobTitle}
                       avatarUrl="https://scontent.fqro1-1.fna.fbcdn.net/v/t1.6435-9/186369854_918866242020897_2260182689203836141_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHkL_3213g0xd3cHo3GbzFnrgJevSH_FgauAl69If8WBlcvBsSeouTzRFBwjnSVm0daqPyCybmVGBLPGSTX-rsK&_nc_ohc=yv4dIO3cflMAX8XO6Ie&tn=B_zKFSPla6C3ZdpL&_nc_ht=scontent.fqro1-1.fna&oh=6fc603851a06bfd86652e02969b413d6&oe=60E5F632"
                     />
                   </div>
       
                   <div className="col-6">
-                    <BadgeForm />
+                    <BadgeForm 
+                      onChange={this.handleChange}
+                      formValues={this.state.form}
+                    />
                   </div>
                 </div>
               </div>
